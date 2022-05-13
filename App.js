256cee3439a2ba959d94a3c874b5160f79e0856e
yannick <yannickkamdemkouam@yahoo.fr>
2022-05-13 11:59:51 +0200

    Init Exercice props

--- App.js
+++ App.js
@@ -1,11 +1,71 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View, TouchableHighlight, Text } from "react-native";
+
+const CustomBtn = (props) => {
+
+  let btnColor = null
+  let textColor = "white"
+
+  if (props.type == "primary") {
+    btnColor = "blue"
+    textColor = "white"
+  }
+
+  if (props.type =="secondary") {
+    btnColor = "gray"
+    textColor = "white"
+  }
+
+  if (props.type == "success") {
+    btnColor = "green"
+    textColor = "white"
+  }
+
+  if (props.type == "danger") {
+    btnColor = "red"
+    textColor = "white"
+  }
+
+  if (props.type == "warning") {
+    btnColor = "yellow"
+    textColor = "black"
+  }
+
+  if (props.type == "info") {
+    btnColor = "aqua"
+    textColor = "black"
+  }
+
+  if (props.type == "light") {
+    btnColor = "#ecf0f1"
+    textColor = "black"
+  }
+
+  if (props.type == "dark") {
+    btnColor = "black"
+    textColor = "white"
+
+  }
+
+  return (
+    
+    <TouchableHighlight style={{backgroundColor : btnColor, height: 50, width: 150, borderRadius: 3, justifyContent: "center",  }}>
+      <Text style={{textAlign: "center", color: textColor, }}>{props.label}</Text>
+    </TouchableHighlight>
+  )
+}
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+
+    <View style ={styles.container}>
+      <CustomBtn type ="primary" label ="Primaire"/>
+      <CustomBtn type ="secondary" label ="Secondaire"/>
+      <CustomBtn type ="success" label ="Succès"/>
+      <CustomBtn type ="danger" label ="Danger"/>
+      <CustomBtn type ="warning" label ="Vigilance"/>
+      <CustomBtn type ="info" label ="Ciel"/>
+      <CustomBtn type ="light" label ="Claire"/>
+      <CustomBtn type ="dark" label ="Sombre"/>
     </View>
   );
 }
@@ -13,8 +73,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: "space-around",
+    alignItems: "center"
   },
 });
